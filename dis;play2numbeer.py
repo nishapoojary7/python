5bfbefa6d60ee8b93d50f8017b46b954c11d0237
Nisha <117558347+nishapoojary7@users.noreply.github.com>
2023-06-20 19:05:39 +0530

    Add files via upload

---
+++ dis;play2numbeer.py
@@ -0,0 +1,6 @@
+
+#read the input from the user
+num1=input("enter the first number : ");
+num2=input("enter the second number : ");
+print("the first number is : " +str(num1));
+print("the second number is :" +str(num2));
