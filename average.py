9eec8629fecfb4ce004b5a67072e09ed1a512d38
Nisha <117558347+nishapoojary7@users.noreply.github.com>
2023-06-30 18:23:37 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,13 @@
+
+m1=int(input("enter the marks of test 1:"))
+m2=int(input("enter the marks of test 2:"))
+m3=int(input("enter the marks of test 3:"))
+if m1<=m2 and m1<=m3:
+   avgmarks=(m2+m3)/2
+elif m2<=m1 and m2<=m3:
+     avgmarks=(m1+m3)/2
+elif m3<=m1 and m3<=m2:
+     avgmarks=(m1+m2)/2
+print("the average of best of 2 test marks out of 3 test is",avgmarks)
+     
+                          
